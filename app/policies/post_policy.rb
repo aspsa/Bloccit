6530bb069c22e79954bf262eea36654a5c366d15
aspsa <123aspsa123@gmail.com>
2015-08-22 21:58:20 +0000

    Assignment #39 complete, Authorization [2015-08-22]

--- app/policies/post_policy.rb
+++ app/policies/post_policy.rb
@@ -38,15 +38,12 @@ class PostPolicy < ApplicationPolicy
     # Need to check with mentor.
     class Scope < Scope
         def resolve_user_role
-            if (user == 'admin')
-                scope.all
-            elsif (user == 'moderator')
-                scope.all
-            elsif (user == 'member')
-                scope.where(user: 'member')
-            # Remaining user role; in this case 'guest'
-            else
-                scope.where(user: 'guest')
+            if user.present?
+                if (user.admin?) || (user.moderator?) # Defined in 'user.rb
+                    scope.all
+                else
+                    scope.where(user_id: user.id)
+                end
             end
         end
     end
