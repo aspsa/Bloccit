7b392abccd93549ecfab977910f1eddfbc96e398
aspsa <123aspsa123@gmail.com>
2015-09-13 04:23:51 +0000

    Assignment #42 - Validating Posts; complete.

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -5,4 +5,11 @@
 class Topic < ActiveRecord::Base
     # Checkpoint #40 - Topics and Posts
     has_many :posts
+    
+    # Assignment #42 - Validating Posts
+    #
+    # In addition to validating posts, we should validate topics.
+    #   Add a validation to ensure that Topic names are at least five characters
+    #       long.
+    validates :name, length: { minimum: 5 }, presence: truep.va
 end
