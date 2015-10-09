b8d9ea6e36d49e9371663a60f8bfa4b52c84b56f
aspsa <123aspsa123@gmail.com>
2015-10-09 14:09:45 +0000

    Assignment #55 - Favoriting; complete.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -12,10 +12,34 @@ describe User do
         # Run the above specs, and they should show up as "pending", because of the xit syntax with which we declare the individual tests. Completing these tests will be your assignment for this checkpoint.
         #
         # Also, see 'http://stackoverflow.com/questions/27288775/how-to-ignore-or-skip-a-test-method-using-rspec'
-        xit "returns 'nil' if the user has not favorited the post" do
+        
+        # Assignment #55 - Favoriting
+        #
+        #   - Complete the user_spec.rb file we scaffolded at the beginning of the checkpoint.
+        #
+        #   - Keep your spec DRY by creating common objects in a before block inside of describe "#favorited(post)".
+        #
+        #   - Don't forget to change xit to it. Unless you do, your tests will be considered "pending" and won't be run.
+        
+        before do
+            @user = authenticated_user  # From 'spec/support/test_factories.rb'
+            @post = associated_post     # From 'spec/support/test_factories.rb'
+        end
+        
+        #xit "returns 'nil' if the user has not favorited the post" do
+        #end
+        it "returns 'nil' if the user has not favorited the post" do
+            expect( @user.favorited(@post) ).to be_nil
         end
        
-        xit "returns the appropriate favorite if it exists" do
+        #xit "returns the appropriate favorite if it exists" do
+        #end
+        it "returns the appropriate favorite if it exists" do
+            # Adapted from 'Favorites#create' ('controllers/favorites_controller.rb')
+            favorite = @user.favorites.build(post: @post)
+            favorite.save
+            
+            expect( @user.favorited(@post) ).not_to be_nil
         end
     end
 end
